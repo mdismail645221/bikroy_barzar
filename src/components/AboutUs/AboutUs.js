9d21df54d5f19a7f09d6ea9d3acf98d648976a22
mdismail645221 <mdismail645221@gmail.com>
2022-11-24 18:09:29 +0600

    about us title added

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -3,7 +3,17 @@ import React from 'react';
 const AboutUs = () => {
     return (
         <section>
-            
+            <div>
+                <div className='about-us-left'>
+                    <div className='text-3xl font-semibold'>
+                        ABOUT US
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div className='about-us-right'>02</div>
+            </div>
         </section>
     );
 };

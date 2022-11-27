85273208e7144111ee7334364a6e68503869e31e
mdismail645221 <mdismail645221@gmail.com>
2022-11-27 21:50:46 +0600

    confirm a payment method added

--- src/pages/Payment/CheckoutForm.js
+++ src/pages/Payment/CheckoutForm.js
@@ -1,40 +1,112 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from '../../components/Loading/Loading';
 
-const CheckoutForm = () => {
+
+const CheckoutForm = ({ paymentInfo }) => {
+    const [loading, setLoading] = useState(false)
+    const [clientSecret, setClientSecret] = useState("");
+    const [paymentError, setPaymentError] = useState(null)
     const stripe = useStripe();
     const elements = useElements();
+    const { resalePrice, userName, email } = paymentInfo;
 
-    const handleSubmit = event => {
+
+
+    useEffect(() => {
+        setLoading(true)
+        // Create PaymentIntent as soon as the page loads
+        fetch(`${process.env.REACT_APP_WEB_LINK}/create-payment-intent`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: `Bearer ${localStorage.getItem("bb_token")}`
+            },
+            body: JSON.stringify(paymentInfo),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setClientSecret(data.clientSecret)
+                setLoading(false)
+            });
+    }, [resalePrice]);
+
+
+
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         if (!stripe || !elements) {
             return;
         }
         const card = elements.getElement(CardElement);
+
+        if (card === null) {
+            return
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        })
+
+        if (error) {
+            console.log(error)
+            setPaymentError(error.message)
+        } else {
+            setPaymentError(null)
+        }
+
+        const { paymentIntent, error:confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: userName,
+                        email: email
+                    },
+                },
+            },
+        );
+
+
+    }
+
+    if(confirmError){
+        setPaymentError(confirmError.message)
+    }
+
+
+    if (loading) {
+        return <Loading></Loading>
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <section>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-primary my-8' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className='btn btn-sm btn-primary my-8' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+            </form>
+            {paymentError && <p>{paymentError}</p>}
+        </section>
     );
 };
 
--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -22,7 +22,7 @@ const Payment = () => {
             <div className='card w-96 bg-base-100 shadow-xl'>
                 <div className='card-body'>
                     <Elements stripe={stripePromise}>
-                        <CheckoutForm />
+                        <CheckoutForm paymentInfo={paymentInfo} />
                     </Elements>
                 </div>
             </div>

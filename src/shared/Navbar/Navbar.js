20009e1887a0b2d6d879a1dcf5e4a4ac8ab42b8f
mdismail645221 <mdismail645221@gmail.com>
2022-12-21 20:40:43 +0600

    navbar gb color change

--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     const menuItems = <>
         <li><NavLink
             to="/"
-            className={`${({ isActive }) => isActive ? "activeStyle" : undefined} inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none`}
+            className={`${({isActive}) => isActive ? "bg-red-300" : undefined} inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none`}
             aria-label="Home"
             title="Home"
         >
